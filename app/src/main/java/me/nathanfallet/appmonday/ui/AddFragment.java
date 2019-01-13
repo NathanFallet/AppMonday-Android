fc750cb922dad0e47897cec600ccfa321853467f
Nathan Fallet <contact@nathanfallet.me>
2019-01-13 13:44:35 +0100

    Link to website to add an app - Native form comming in next update

--- app/src/main/java/me/nathanfallet/appmonday/ui/AddFragment.java
+++ app/src/main/java/me/nathanfallet/appmonday/ui/AddFragment.java
@@ -1,10 +1,14 @@
 package me.nathanfallet.appmonday.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import me.nathanfallet.appmonday.R;
 
@@ -22,7 +26,19 @@ public class AddFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        View v = inflater.inflate(R.layout.fragment_add, container, false);
+
+        TextView open = v.findViewById(R.id.open_web);
+        open.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.appmonday.xyz")));
+
+                return true;
+            }
+        });
+
+        return v;
     }
 
 }
